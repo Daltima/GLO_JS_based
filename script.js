4f4fbd241b346a7fb531518b22a837854e4db949
Alla Dulia <alla13projects@gmail.com>
2020-11-22 23:04:14 +0200

    Version01

---
+++ script.js
@@ -0,0 +1,19 @@
+let money = 30000;
+let income = 'фриланс';
+let addExpenses = 'Интернет, такси, коммуналка';
+let deposit = true;
+let mission = 150000;
+let period = 12; 
+
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен '+ period + ' месяцев.');
+console.log('Цель заработать '+ mission + ' гривен.');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+let budgetDay = 1000;
+console.log(budgetDay);
