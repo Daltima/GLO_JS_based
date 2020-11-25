cec3276e9d42d952465b012327a89745dc021a5d
Alla Dulia <alla13projects@gmail.com>
2020-11-25 21:43:10 +0200

    Version02: budgetDay was changed

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-let money = 30000;
+'use strict';
+
+let money = +prompt('Ваш месячный доход?', 50000);
 let income = 'фриланс';
-let addExpenses = 'Интернет, такси, коммуналка';
-let deposit = true;
-let mission = 150000;
-let period = 12; 
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую',
+'такси, коммунальные, питание, обучение');
+let deposit = confirm('Есть ли у вас депозит в банке?');
+const mission = 150000;
+const period = 12; 
 
 console.log(typeof money);
 console.log(typeof income);
@@ -14,19 +17,11 @@ console.log('Цель заработать '+ mission + ' гривен.');
 console.log(addExpenses.toLowerCase());
 console.log(addExpenses.split(', '));
 
-let budgetDay = money/30;
-console.log(budgetDay);
+let expenses1 = prompt('Введите обязательную статью расходов?', 'коммунальные');
+let amount1 = +prompt('Во сколько это обойдется?', 5400);
 
-money = +prompt('Ваш месячный доход?');
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-deposit = confirm('Есть ли у вас депозит в банке?');
-
-let expenses1 = prompt('Введите обязательную статью расходов?');
-let amount1 = +prompt('Во сколько это обойдется?');
-
-let expenses2 = prompt('Введите обязательную статью расходов?');
-let amount2 = +prompt('Во сколько это обойдется?');
-
+let expenses2 = prompt('Введите обязательную статью расходов?', 'питание');
+let amount2 = +prompt('Во сколько это обойдется?', 21600);
 
 let budgetMonth = money - amount1 - amount2;
 console.log('Бюджет на месяц: ' + budgetMonth);
@@ -34,14 +29,14 @@ console.log('Бюджет на месяц: ' + budgetMonth);
 let missionPeriod = Math.ceil(mission/budgetMonth);
 console.log('Цель будет достигнута за ' + missionPeriod + ' месяцев');
 
-budgetDay = Math.floor(budgetMonth/missionPeriod);
+let budgetDay = Math.floor(budgetMonth/30);
 console.log('Бюджет на день ' + budgetDay);
 
-if(budgetDay > 1200 || budgetDay === 1200) {
+if(budgetDay >= 1200) {
     console.log('У Вас высокий уровень дохода');
 } else if(budgetDay > 600 && budgetDay < 1200) {
     console.log('У Вас средний уровень дохода');
-} else if(budgetDay < 600 && budgetDay < 0 || budgetDay === 600  || budgetDay === 0) {
+} else if(budgetDay <= 600 && budgetDay <= 0) {
     console.log('К сожалению у вас уровень дохода ниже среднего');
 } else {
     console.log('Что то пошло не так');
