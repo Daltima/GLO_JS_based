ccd4feb4c2ffe676ec8abf03d8d2fb50d8c2d0c0
Alla Dulia <alla13projects@gmail.com>
2021-02-01 22:22:15 +0200

    Orginized code in script.js

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 'use strict';
 
-let money = +prompt('Ваш месячный доход?', 50000);
-let income = 'фриланс';
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую',
-'такси, коммунальные, питание, обучение');
-let deposit = confirm('Есть ли у вас депозит в банке?');
-const mission = 150000;
-const period = 12; 
+let money = +prompt('Ваш месячный доход?', 50000),
+income = 'фриланс',
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую',
+'такси, коммунальные, питание, обучение'),
+deposit = confirm('Есть ли у вас депозит в банке?');
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+const mission = 150000,
+period = 12; 
+
+let expenses1 = prompt('Введите обязательную статью расходов?', 'коммунальные'),
+amount1 = +prompt('Во сколько это обойдется?', 5400),
+
+expenses2 = prompt('Введите обязательную статью расходов?', 'питание'),
+amount2 = +prompt('Во сколько это обойдется?', 21600),
+
+budgetMonth = money - amount1 - amount2,
+missionPeriod = Math.ceil(mission/budgetMonth),
+budgetDay = Math.floor(budgetMonth/30);
+
+console.log(typeof money, typeof income, typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен '+ period + ' месяцев.');
 console.log('Цель заработать '+ mission + ' гривен.');
-console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split(', '));
-
-let expenses1 = prompt('Введите обязательную статью расходов?', 'коммунальные');
-let amount1 = +prompt('Во сколько это обойдется?', 5400);
-
-let expenses2 = prompt('Введите обязательную статью расходов?', 'питание');
-let amount2 = +prompt('Во сколько это обойдется?', 21600);
-
-let budgetMonth = money - amount1 - amount2;
+console.log(addExpenses.toLowerCase().split(', '));
 console.log('Бюджет на месяц: ' + budgetMonth);
-
-let missionPeriod = Math.ceil(mission/budgetMonth);
 console.log('Цель будет достигнута за ' + missionPeriod + ' месяцев');
-
-let budgetDay = Math.floor(budgetMonth/30);
 console.log('Бюджет на день ' + budgetDay);
 
 if(budgetDay >= 1200) {
