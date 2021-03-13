2f6a4a0c6b012fb126f89e137a91b8adf2d91dcd
Alla Dulia <alla13projects@gmail.com>
2021-03-13 11:25:42 +0200

    the styles were connected with div and p

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ function DomElement (selector, height, width, bg, fontSize) {
     this.bg = bg;
     this.fontSize = fontSize;
 }
+
+const element = new DomElement('.first', '120px', '300px', 'yellow', '40px');
+const elementP = new DomElement('#second', '120px', '300px', 'blue', '40px');
+
 //Метод при котором, если строка selector начинается с точки, создаем div с классом
 // если строка selector  начинается с решетки # то создаем параграф с id
 //название id или класса, является строка selector без первого символа
@@ -17,23 +21,35 @@ DomElement.prototype.createElm = function() {
 
         let elm = document.createElement('div');
         elm.classList = this.selector.substring(1);
-        console.log('elm.id : ', elm.id );
-        
-        document.body.appendChild(elm);
-        elm.innerText = "Some string";
+        elm.style.cssText = `
 
+            height: ${this.height};
+            width: ${this.width};
+            background: ${this.bg};
+            font-size: ${this.fontSize};
+        `;
+        // elm.style.fontSize = this.fontSize;
+        elm.textContent = "Some string";
+        document.body.appendChild(elm);
+       
     } else if (this.selector.charAt(0) === "#") {
 
         let elm2 = document.createElement("p");
         elm2.id = this.selector.substring(1);
+        elm2.style.cssText = `
+
+            height: ${this.height};
+            width: ${this.width};
+            background: ${this.bg};
+            font-size: ${this.fontSize};
+        `;
+        elm2.textContent = "Some string2";
         document.body.appendChild(elm2);
-        elm2.innerText = "Some string2";
     } 
     else {
         alert ("Первый символ строки должен быть . или #. Повторите ввод.");
     }
 };
-
-const element = new DomElement("#first", {height: "20px"}, {width: "40px"}, {background: "yellow"}, {fontSize: "8px"});
-console.log(element.selector.charAt(0));
 element.createElm();
+elementP.createElm();
+
